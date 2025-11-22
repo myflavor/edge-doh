232a5306f62258d970d4dfffba805eb2618bd1cb
Xin <admin@myflv.cn>
2025-11-22 15:22:43 +0800

    1

--- node-functions/dns-query.js
+++ node-functions/dns-query.js
@@ -5,6 +5,8 @@ const dohUrl = new URL('https://cloudflare-dns.com/dns-query')
 const dohOrigin = dohUrl.origin
 const dohHost = dohUrl.host
 
+const cname = 'cf.100172.xyz'
+
 const fetchCloudflareDns = async context => {
     const request = context.request
     const proxyHeaders = new Headers(request.headers)
@@ -34,6 +36,30 @@ const fetchCloudflareIpv4 = async () => {
     return {response, cidrList}
 }
 
+const fetchAnswers = async () => {
+    const response = await fetch(dohUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/dns-message'
+        },
+        body: dnsPacket.encode({
+            type: 'query',
+            id: 1,
+            flags: dnsPacket.RECURSION_DESIRED,
+            questions: [{
+                type: 'A',
+                name: cname
+            }]
+        })
+    })
+    
+    const bytes = await response.bytes()
+    const buffer = Buffer.from(bytes)
+    
+    const packet = dnsPacket.decode(buffer)
+    return packet.answers
+}
+
 const proxyRequest = async context => {
     
     const promises = []
@@ -41,36 +67,76 @@ const proxyRequest = async context => {
     promises.push(fetchCloudflareIpv4())
     const [dnsResult, ipv4Result] = await Promise.all(promises)
     
+    const response = new Response(dnsResult.buffer, {
+        status: dnsResult.response.status,
+        statusText: dnsResult.response.statusText,
+        headers: dnsResult.response.headers
+    })
+    
     try {
         const packet = dnsPacket.decode(dnsResult.buffer)
         
         console.log('packet',packet)
+        if (packet.questions == null) {
+            return response
+        }
         
-        if (packet.answers){
+        if (packet.questions.length !== 1) {
+            return response
+        }
+        
+        if (packet.answers == null) {
+            return response
+        }
+        const question = packet.questions[0]
+        
+        if (question.type !== 'A') {
+            return response
+        }
+        if (question.class !== 'IN') {
+            return response
+        }
+        if (question.name === cname) {
+            return response
+        }
+        
+        let cloudflare = false
+        
+        for (let i = 0; i < packet.answers.length; i++) {
+            const answer = packet.answers[i]
+            if (answer.type === 'A') {
+                const ipv4 = answer.data
+                const addr =   ipaddr.parse(ipv4)
+                for (let cidr of ipv4Result.cidrList) {
+                    if (addr.match(ipaddr.parseCIDR(cidr))){
+                        cloudflare = true
+                    }
+                }
+                
+            }
+        }
+        
+        if (cloudflare) {
+            const answers = await fetchAnswers()
+            if (answers == null || answers.length === 0){
+                return response
+            }
+            
+            packet.answers = answers
             for (let i = 0; i < packet.answers.length; i++) {
                 const answer = packet.answers[i]
-                if (answer.type === 'A') {
-                    const ipv4 = answer.data
-                    
-                    const addr =   ipaddr.parse(ipv4)
-                    for (let cidr of ipv4Result.cidrList) {
-                        if (addr.match(ipaddr.parseCIDR(cidr))){
-                            answer.type = 'CNAME'
-                            answer.data = 'cf.100172.xyz'
-                            break
-                        }
-                    }
-                    
+                if (answer.type === 'CNAME' && answer.name === cname) {
+                    answer.name = question.name
                 }
             }
+            
             const proxyHeaders = new Headers(dnsResult.response.headers)
             proxyHeaders.delete('content-length')
-            
-           return new Response(dnsPacket.encode(packet), {
-            status: dnsResult.response.status,
-            statusText: dnsResult.response.statusText,
-            headers: proxyHeaders
-        })
+            return new Response(dnsPacket.encode(packet), {
+                status: dnsResult.response.status,
+                statusText: dnsResult.response.statusText,
+                headers: proxyHeaders
+            })
         }
         
         
@@ -78,11 +144,7 @@ const proxyRequest = async context => {
         console.log('dns解析出错', error)
     }
     
-    return new Response(dnsResult.buffer, {
-        status: dnsResult.response.status,
-        statusText: dnsResult.response.statusText,
-        headers: dnsResult.response.headers
-    })
+    return response
     
 }
 
